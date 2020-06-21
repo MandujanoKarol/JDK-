0e2a1fc1595b98f43318392a0ff222cb2d446e19
Juancruzd <42796029+Juancruzd@users.noreply.github.com>
2020-06-21 11:53:24 -0500

    modificacion de registro

--- js/service/Usuario/auth/register_User.js
+++ js/service/Usuario/auth/register_User.js
@@ -1,58 +1,4 @@
-////DOM form  User
-const formRegisterUser = document.getElementById('formRegisterUser');
-////DOM form  User evento submit button register
-/*formRegisterUser.addEventListener('submit',(e)=>{
-    e.preventDefault(); 
-    //data
-    const email = formRegisterUser['remail'].value;
-    const password = formRegisterUser['rpassword'].value;  
-    ////if the navigator containe geolocation
-    if (navigator.geolocation) {
-        ///create User With Email And Password
-        auth.createUserWithEmailAndPassword(email,password).then( cred =>{ 
-            ///clear form
-            formRegisterUser.reset();   
-            ///Almacenar key del actual usuario logueado en localStorage 
-            localStorage.removeItem("uid");
-            localStorage.setItem("uid", cred.user.uid);
-            navigator.geolocation.getCurrentPosition(function(position) { 
-                var coordenadas = {
-                    Latitud: position.coords.latitude, 
-                    Longitud: position.coords.longitude
-                } 
-                return db.collection('usuarios').doc(cred.user.uid).set({
-                    "nombre": formRegisterUser['rnombre'].value,
-                    "telefono": formRegisterUser['rtelefono'].value,
-                    "correo": formRegisterUser['rcorreo'].value,
-                    "direccion": formRegisterUser['rdireccion'].value,
-                    "coordenadas":coordenadas,
-                    "dateInOut":new Date().toLocaleString(),
-                    "estado":parseInt(1)
-                }).then(function() { 
-                    formRegisterUser.reset();
-                    document.getElementById('errorregistrar').innerHTML = ''; 
-                   // window.location.replace("index.html");
-                    //window.location.replace("https://juancruzd.github.io/Practica1-Sistemas-Geo-Referenciados/firebase/practica2/index.html");
-                }).catch(function(error) {
-                        console.error("Error regitering document: ", error);
-                });
-            }, function(error) {
-                console.log(error);
-            });
-        
-        
-    }).then( ()=>{ 
-        
-    }).catch( err => {
-        console.log(err);
-        document.getElementById('errorregistrar').innerHTML = mensajeError(err.code);
-    });
-    } else {
-        console.log("error ubicacion");
-    }
-    
 
-});*/
 
 ///carga cuando el documento ya este listo
 $(document).ready(function() {  
@@ -69,149 +15,119 @@ $(document).ready(function() {
                 // si la solicitud fue exitosa
                 if (status === google.maps.GeocoderStatus.OK) {
                     // si encontró algún resultado.
-                    if (results[1]) {
-                    console.log(results[1]);  
+                    if (results[1]) {  
                     document.forms["formRegisterUser"]["direccion"].value =results[1].formatted_address; 
                     }
                 }
-            });
-            console.log(latlng);
-        }, function(error) {
-            console.log(error);
+            }); 
+        }, function(error) {  
+            floatingMessage(error.title,error.message,"error");
         });
     }
-    else {
-        console.log("error ubicacion");
+    else { 
+        floatingMessage("error al obtener las coordenadas","error ubicacion","error");
     }
 
  });
-function validate() {
-    
-    var errores=0;  
-        ///Nombre
-        if (document.forms["formRegisterUser"]["nombre"].value == "") { 
-            printerrorform("nombre","has-error")
-            document.getElementById("error_nombre").innerHTML = "enter the firstname";
-            errores++;
-        }
-        else if(!/^[a-zA-Z]*$/g.test(document.formRegisterUser.nombre.value))
-        { 
-            //alert("hello");
-            printerrorform("nombre","has-warning");
-            document.getElementById('error_nombre').innerHTML="only alphanumeric character  required";
-            document.formRegisterUser.nombre.focus();
-            return false;
-        }
-        else{
-           document.getElementById("error_nombre").innerHTML = "";
-           printerrorform("nombre","has-success")
-        } 
-
-        ///Apellido
-        if (document.forms["formRegisterUser"]["apellido"].value == "") { 
-            printerrorform("apellido","has-error")
-            document.getElementById("error_apellido").innerHTML = "Ingrese el apellido";
-        errores++;
-        }
-        else if(!/^[a-zA-Z]*$/g.test(document.formRegisterUser.apellido.value))
-        {  
-            printerrorform("apellido","has-warning");
-            document.getElementById('error_apellido').innerHTML="only alphanumeric character  required";
-            document.formRegisterUser.apellido.focus(); 
-            return false;
-        }
-        else{
-           document.getElementById("error_apellido").innerHTML = "";
-           printerrorform("apellido","has-success")
-        } 
-        ///Correo electronico
-        const testcorreo = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        if (document.forms["formRegisterUser"]["correo"].value == "") { 
-            printerrorform("correo","has-error")
-            document.getElementById("error_correo").innerHTML = "Ingrese el correo";
-        errores++;
-        }else if(testcorreo.test(document.formRegisterUser.correo.value.toLowerCase()))
-        {  
-            document.getElementById("error_correo").innerHTML = "";
-            printerrorform("correo","has-success");
-        }
-        else{ 
-            printerrorform("correo","has-warning");
-            document.getElementById('error_correo').innerHTML="ingrese un correo valido ej. example@email.com";
-            document.formRegisterUser.correo.focus(); 
-            errores++;
-            return false;
-        } 
-        ///contrasena
-        const testcontrasena=/(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
-        if (document.forms["formRegisterUser"]["contrasena"].value == "") { 
-            printerrorform("contrasena","has-error")
-            document.getElementById("error_contrasena").innerHTML = "Ingrese la contrasena";
-            errores++;
-        } 
-        else if(testcontrasena.test(document.formRegisterUser.contrasena.value)){  
-            document.getElementById("error_contrasena").innerHTML = "";
-            printerrorform("contrasena","has-success");
-        }
-        else{ 
-            printerrorform("contrasena","has-warning");
-            document.getElementById('error_contrasena').innerHTML="ingrese una contrasena valida";
-            document.formRegisterUser.contrasena.focus(); 
-            errores++; 
-            return false;
-        } 
-        ///contrasena Repetida 
-        if (document.forms["formRegisterUser"]["rcontrasena"].value == "") { 
-            printerrorform("rcontrasena","has-error")
-            document.getElementById("error_rcontrasena").innerHTML = "Repita la contrasena";
-            errores++;
-        }
-        else if (document.forms["formRegisterUser"]["rcontrasena"].value == document.forms["formRegisterUser"]["contrasena"].value) { 
-            document.getElementById("error_rcontrasena").innerHTML = "";
-            printerrorform("rcontrasena","has-success");
-        }  
-        else{ 
-            printerrorform("rcontrasena","has-warning")
-            document.getElementById("error_rcontrasena").innerHTML = "Ambas contrasenas deben coincidir";
-            errores++;
-            return false;
-        }  
-        ///telefono
-        const testtelefono=/^[(]{0,1}[0-9]{3}[)]{0,1}[-\s\.]{0,1}[0-9]{3}[-\s\.]{0,1}[0-9]{4}$/;
-        if (document.forms["formRegisterUser"]["telefono"].value == "") { 
-            printerrorform("telefono","has-error")
-            document.getElementById("error_telefono").innerHTML = "Ingrese el telefono";
-            errores++;
-        } 
-        else if(testtelefono.test(document.formRegisterUser.telefono.value)){  
-            document.getElementById("error_telefono").innerHTML = "";
-            printerrorform("telefono","has-success");
-        }
-        else{ 
-            printerrorform("telefono","has-warning");
-            document.getElementById('error_telefono').innerHTML="ingrese una telefono valido";
-            document.formRegisterUser.telefono.focus(); 
-            errores++; 
-            return false;
-        } 
-        ///Direccion
-        if (document.forms["formRegisterUser"]["direccion"].value == "") { 
-            printerrorform("direccion","has-error")
-            document.getElementById("error_direccion").innerHTML = "enter the direccion";
-            errores++;
-        } 
-        else{
-           document.getElementById("error_direccion").innerHTML = "";
-           printerrorform("direccion","has-success"); 
-        } 
-        ///si hay errores
-        if(errores!=0){ 
-            return false;
-        }
-    
+ ////onkeyups
+function onkeyupnombre(){ 
+    if (document.forms["formRegisterUser"]["nombre"].value == "") { 
+        printerrorform("nombre","has-error")
+        document.getElementById("error_nombre").innerHTML = "Ingrese el nombre"; 
+        return 1;
+    }
+    else if(!/^[a-zA-Z ]{2,30}$/.test(document.formRegisterUser.nombre.value))
+    {  
+        printerrorform("nombre","has-warning");
+        document.getElementById('error_nombre').innerHTML="Ingrese un nombre correcto";
+        document.formRegisterUser.nombre.focus(); 
+        return 1;
+    }
+    else{
+       document.getElementById("error_nombre").innerHTML = "";
+       printerrorform("nombre","has-success");
+       return 0;
+    } 
 }
-
-function separartelefono(){
+function onkeyupapellido(){ 
+    ///Apellido
+    if (document.forms["formRegisterUser"]["apellido"].value == "") { 
+        printerrorform("apellido","has-error")
+        document.getElementById("error_apellido").innerHTML = "Ingrese el apellido"; 
+        return 1;
+    }
+    else if(!/^[a-zA-Z ]{2,30}$/.test(document.formRegisterUser.apellido.value))
+    {  
+        printerrorform("apellido","has-warning");
+        document.getElementById('error_apellido').innerHTML="Ingrese un apellido correcto";
+        document.formRegisterUser.apellido.focus();  
+        return 1;
+    }
+    else{
+       document.getElementById("error_apellido").innerHTML = "";
+       printerrorform("apellido","has-success");
+       return 0;
+    } 
+}
+function onkeyupemail(){ 
+     ///Correo electronico
+     const testcorreo = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+     if (document.forms["formRegisterUser"]["correo"].value == "") { 
+         printerrorform("correo","has-error")
+         document.getElementById("error_correo").innerHTML = "Ingrese el correo"; 
+         return 1;
+     }else if(testcorreo.test(document.formRegisterUser.correo.value.toLowerCase()))
+     {  
+         document.getElementById("error_correo").innerHTML = "";
+         printerrorform("correo","has-success");
+         return 0;
+     }
+     else{ 
+         printerrorform("correo","has-warning");
+         document.getElementById('error_correo').innerHTML="ingrese un correo valido ej. example@email.com";
+         document.formRegisterUser.correo.focus();  
+         return 1;
+     } 
+}
+function onkeyuppassword(){ 
+    ///contrasena
+    const testcontrasena=/(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+    if (document.forms["formRegisterUser"]["contrasena"].value == "") { 
+        printerrorform("contrasena","has-error")
+        document.getElementById("error_contrasena").innerHTML = "Ingrese la contrasena"; 
+        return 1;
+    } 
+    else if(testcontrasena.test(document.formRegisterUser.contrasena.value)){  
+        document.getElementById("error_contrasena").innerHTML = "";
+        printerrorform("contrasena","has-success");
+        return 0;
+    }
+    else{ 
+        printerrorform("contrasena","has-warning");
+        document.getElementById('error_contrasena').innerHTML="ingrese una contrasena valida";
+        document.formRegisterUser.contrasena.focus();  
+        return 1;
+    }
+}
+function onkeyuprpassword(){ 
+    ///contrasena Repetida 
+    if (document.forms["formRegisterUser"]["rcontrasena"].value == "") { 
+        printerrorform("rcontrasena","has-error")
+        document.getElementById("error_rcontrasena").innerHTML = "Repita la contrasena"; 
+        return 1;
+    }
+    else if (document.forms["formRegisterUser"]["rcontrasena"].value == document.forms["formRegisterUser"]["contrasena"].value) { 
+        document.getElementById("error_rcontrasena").innerHTML = "";
+        printerrorform("rcontrasena","has-success");
+        return 0;
+    }  
+    else{ 
+        printerrorform("rcontrasena","has-warning")
+        document.getElementById("error_rcontrasena").innerHTML = "Ambas contrasenas deben coincidir"; 
+        return 1;
+    }  
+}
+function onkeyuptelefono(){
     if (document.forms["formRegisterUser"]["telefono"].value != "") {    
         if(document.forms["formRegisterUser"]["telefono"].value.length ==10){  
             var phoneNumberString=document.forms["formRegisterUser"]["telefono"].value 
@@ -221,19 +137,112 @@ function separartelefono(){
                 document.getElementById("error_telefono").innerHTML = "";
                 printerrorform("telefono","has-success");
                 document.forms["formRegisterUser"]["telefono"].value= '(' + match[1] + ') ' + match[2] + '-' + match[3]
-                
+                return 0;
             }
         } else{ 
             if(document.forms["formRegisterUser"]["telefono"].value.length ==14){ 
                 document.getElementById("error_telefono").innerHTML = "";
                 printerrorform("telefono","has-success");
-             }else{
+                return 0;
+             }else{ 
                 printerrorform("telefono","has-error");
+                return 1;
             }
             
         }
     }
 } 
+function onkeyupdireccion(){ 
+    ///Direccion
+    if (document.forms["formRegisterUser"]["direccion"].value == "") { 
+        printerrorform("direccion","has-error")
+        document.getElementById("error_direccion").innerHTML = "Ingrese la direccion"; 
+        return 1;
+    } 
+    else{
+       document.getElementById("error_direccion").innerHTML = "";
+       printerrorform("direccion","has-success"); 
+       return 0;
+    }
+}
+ ////Funcion register
+function register(){
+    //data
+    const email = document.forms["formRegisterUser"]['correo'].value;
+    const password = document.forms["formRegisterUser"]['contrasena'].value;  
+    ////if the navigator containe geolocation
+    if (navigator.geolocation) { 
+        ///create User With Email And Password
+        auth.createUserWithEmailAndPassword(email,password).then( cred =>{ 
+            ///clear form
+            document.forms["formRegisterUser"].reset();   
+            ///Almacenar key del actual usuario logueado en localStorage 
+            localStorage.removeItem("uid");
+            localStorage.setItem("uid", cred.user.uid);
+            ///get position
+            navigator.geolocation.getCurrentPosition(function(position) { 
+                ///cords
+                var coordenadas = {
+                    Latitud: position.coords.latitude, 
+                    Longitud: position.coords.longitude
+                } 
+                //registrar nuevos datos en firebase database with id user auth
+                return db.collection('usuarios').doc(cred.user.uid).set({
+                    "nombre": document.forms["formRegisterUser"]['nombre'].value,
+                    "apellido": document.forms["formRegisterUser"]['apellido'].value,
+                    "correo":document.forms["formRegisterUser"]['correo'].value,
+                    "telefono":document.forms["formRegisterUser"]['telefono'].value,
+                    "direccion": document.forms["formRegisterUser"]['direccion'].value,
+                    "coordenadas":coordenadas,
+                    "fechaRegistro":new Date().toLocaleString(),
+                    "estado":parseInt(1)
+                }).then(function() { 
+                    formRegisterUser.reset();  
+                }).catch(function(error) {
+                    floatingMessage(error.code,"","firebase");
+                });
+
+            }, function(error) {
+                ////error al obtener coordenadas
+                floatingMessage(error.title,error.message,"error");
+            });
+        }).catch( err => {  
+            floatingMessage(err.code,"","firebase");
+        });
+
+
+    } else {
+        floatingMessage("error al obtener las coordenadas","error ubicacion","error");
+    }
+    
+
+};
+////Funcion validar campos forms
+function validarForms() {
+    var errores=0;  
+    errores+=onkeyupnombre();
+    errores+=onkeyupapellido();
+    errores+=onkeyupemail();
+    errores+=onkeyuppassword();
+    errores+=onkeyuprpassword();
+    errores+=onkeyuptelefono();
+    errores+=onkeyupdireccion(); 
+    if(errores==0){ 
+    return true;
+    }else{
+    return false;
+    }
+};
+////funcion click button
+function validator(){ 
+    console.log(validarForms().toString());
+    if(validarForms().toString()=="true"){
+        register();
+    }else{
+
+    }
+}
+
 var input = document.querySelector("#phone");
   window.intlTelInput(input, {
     // any initialisation options go here
--- js/service/login.js
+++ js/service/login.js
@@ -1,31 +1,3 @@
-///funcion de mensaje de error
-function mensajeError(codigo) { 
-    let mensaje = ''; 
-    switch(codigo) {
-        case 'auth/wrong-password':
-          mensaje = 'Su contraseña no es correcta';
-          break;
-        case 'auth/user-not-found':
-            mensaje = 'El usuario no existe o el correo no esta registrado';
-            break;
-        case 'auth/weak-password':
-            mensaje = 'Contraseña débil debe tener al menos 6 caracteres';
-            break;
-        case 'auth/network-request-failed':
-            mensaje = 'A network error (such as timeout, interrupted connection or unreachable host) has occurred.';
-            break;
-        case 'auth/weak-password':
-            mensaje = 'Password should be at least 6 characters';
-            break;
-        case 'auth/email-already-in-use':
-            mensaje = 'The email address is already in use by another account.';
-            break;
-        default:
-            mensaje = 'Ocurrió un error al ingresar con este usuario';
-      }
-    return mensaje;
-}
-
 ////DOM form  LogIn
 const formLogIn =  document.getElementById('formLogIn'); 
 ////DOM form  LogIn event  submit on button LogIn
